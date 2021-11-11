8cb05a283644bb565dc2cf436c513c1b6afbe9ae
PhilipJanson <ph8671ja-s@student.lu.se>
2021-11-11 11:51:25 +0100

    Labb1

---
+++ pi/joystick_controller.py
@@ -0,0 +1,51 @@
+import requests
+import time
+import random
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+def get_direction():
+    d_long = 0
+    d_la = 0
+    send_vel = False
+    
+    for event in sense.stick.get_events():     
+        c = event.direction
+        action = event.action
+        
+        if (action == 'held' or action == 'pressed') and c =='left':
+            print('Left')
+            send_vel = True
+            d_long = -1
+            d_la = 0
+        elif (action == 'held' or action == 'pressed') and c == 'right':
+            print('Right')
+            send_vel = True
+            d_long = 1
+            d_la = 0
+        elif (action == 'held' or action == 'pressed') and c =='up':
+            print('Up')
+            send_vel = True
+            d_long = 0
+            d_la = 1
+        elif (action == 'held' or action == 'pressed') and c == 'down':
+            print('Down')
+            send_vel = True
+            d_long = 0
+            d_la = -1
+
+    return d_long, d_la, send_vel
+
+
+if __name__ == "__main__":
+    SERVER_URL = "http://127.0.0.1:5000/drone"
+    while True:
+        d_long, d_la, send_vel = get_direction()
+        if send_vel:
+            with requests.Session() as session:
+                current_location = {'longitude': d_long,
+                                    'latitude': d_la
+                                    }
+                resp = session.post(SERVER_URL, json=current_location)
+
